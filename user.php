2befd9078064323ff044dc4f3ecfb5f2593fd994
Remedee <zacharydanieldowns@gmail.com>
2019-02-23 15:48:07 -0600

    Created the files for the userpage
    Just making the files. Also renamed the folder Homepage to Home

---
+++ user.php
@@ -0,0 +1,9 @@
+html>
+<head>
+	<title>Skopos Security - User</title>
+	<link rel="stylesheet" type="text/css" href="register.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+</head>
+<body style="background-color:rgba(0,0,0,0);">
+</body>
+</html>
