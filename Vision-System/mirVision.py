572d7c25be55f539f5cc55a7123801ed03ed1686
Remedee <zacharydanieldowns@gmail.com>
2019-03-22 19:00:39 -0500

    Finished NameGen() + New Functions GenHelper() and Delete()
    The NameGen() function is finally finished supporting making unique file names for footage, images, and clips. The function GenHelper() was made to not repeat code needed to get a list of the footage directory. Delete() was introduced to deal with removing old footage. The function should be operable with deleting images and clips as well, but needs testing.

--- Vision-System/mirVision.py
+++ Vision-System/mirVision.py
@@ -27,6 +27,8 @@
 # +Entering of high alert mode if clip reaches 15 minutes.
 # +Queueing clip for deletion as soon as upload is verfied in high alert mode.
 
+
+
 ###
 ### Initializtation section.
 ###
@@ -38,7 +40,7 @@ import sys # Used for exiting upon error.
 import os # For changing the directory.
 
 # Define limit on the numbers of file that can be made by type.
-FOOTAGELIM = 5
+FOOTAGELIM = 2
 IMAGELIM = 30
 CLIPLIM = 10
 
@@ -52,9 +54,34 @@ username = "fake" # Hard coding the user account for our prototype.
 codec = cv2.VideoWriter_fourcc('M','J','P','G') # Define the codec used to compress video files.
 
 
+
 ###
 ### Camera operation functions.
 ###
+
+# Deletes the given file or first file in the given list.
+def Delete(file):
+
+    # If a list then get its first element.
+    if isinstance(file, list):
+        file.sort()
+        file = file[0]
+
+    # Determine what path to delete from.
+    if "footage" in file:
+        path = "Footage/"
+    elif "image" in file:
+        path = "Images/"
+    elif "clip" in file:
+        path = "Clips/"
+    else:
+        print("Cant delete a file of this type.")
+        sys.exit(1) # If no path matches then exit with error.
+
+    os.chdir(path) # Change wd to path.
+    os.system("rm -f " + file) # Remove the specified file.
+    os.chdir("..") # Return to original directory.
+
 
 # Returns a list of a users directory.
 def UserDir(extension):
@@ -67,65 +94,77 @@ def UserDir(extension):
     dirlist = [file for file in dirlist if extension in file] # Removes all files not containing the desired extension.
     return dirlist
 
+
+# Helps NameGen by not repeating footage fle code.
+def GenHelper():
+
+    # Grab a list of files from footage diretory.
+    for files in os.walk("Footage"):
+        for filename in files:
+            if filename  is not ".":
+                file = filename
+
+    return file
+
+
 # Geneates a unique file name and returns it.
 def NameGen(type, extension):
     
     i = 0 # Incrimentor variable.
-    found = False # Whether a name could be generated.
 
     if type == "footage": # If of type footage make cwd Footage
-        os.chdir("Footage")
-        n = FOOTAGELIM # n is used to set a loop limit for later in the function.
+        file = GenHelper() # Get files in the footage directory.
+        n = FOOTAGELIM # n is used to set the file limit for the given type.
+
+        # If file is of size n then delete footage0, rename footage1 to footage0, then re-get files.
+        if len(file) == n:
+            file.sort()
+            Delete(file)
+            os.system("mv Footage/footage1.mkv Footage/footage0.mkv")
+            file = GenHelper()
 
     elif type == "clip": # If of type clip make cwd Clips
-        os.chdir("Clips")
+        file = UserDir(extension) # Grabs file list from server.
         n = CLIPLIM
 
     elif type == "image": # If of type image make cwd Images
-        os.chdir("Images")
+        file = UserDir(extension)
         n = IMAGELIM
 
     else:
         print ("Error creating file name.")
         sys.exit(1) # If none of the above exit with error.
     
-    # Grab a list of files from current diretory.
-    for files in os.walk("."):
-        for filename in files:
-            if filename  is not ".":
-                file = filename
+    # If the file limit is reached then exit with error.
+    if len(file) == n :
+        print("No more space to generate files.")
+        sys.exit(1)
 
     file.sort() # Sort file list alphabetically
 
     # Generate an unused filename.
     for i in range(n):
 
-        # If all files have been checked and i is less than the limit generate a name then break.
-        if i >= len(file) and i < n:
+        # If all files have been checked generate a name then break.
+        if i == len(file):
             namegen = type + str(i) + "." + extension
-            found = True
             break
 
         namegen = type + str(i) + '.' + extension # Generate a file name.
 
-        # If the generated file name does not exist then use it an break.
+        # If the generated file name does not exist then use it and break.
         if namegen != file[i]:
-            found = True
             break
 
-    if found == True:
-        os.chdir("..") # Return to the original wd.
-        return namegen
-    else:
-        print("No more space to generate files.")
-        sys.exit(1)
+    return namegen
 
 # Starts the recording of 5 minute footage segments.
 def Record():
 
+    outfile = NameGen("footage", "mkv") # Grab an unused file name.
     os.chdir("Footage") # Write to "the footage folder.
     camera = cv2.VideoCapture(0) # Set up a video feed from the camera
-    vidout = cv2.VideoWriter('output.mkv', codec, 30, (640,480)) # File to write footage to at 30 fps.
+    vidout = cv2.VideoWriter(outfile, codec, 30, (640,480)) # File to write footage to at 30 fps.
     timer = time.time() + 300 # Set a 5 minute timer in seconds to record footage clip
     
     # Write video data while camera is recording and recording is less than 5 minutes.
@@ -141,11 +180,13 @@ def Record():
 
     stopRecord(camera, vidout) # End the recording.
 
+
 # When called removes recording components.
 def stopRecord(camera, vidout):
     camera.release() # Release the camera.
     vidout.release() # Release the video file.
     os.chdir("..") # Return to the files directory.
+
 
 # Uploads images and videos to a user's folder on the server.
 def Uplaod(filename, extension):
@@ -156,12 +197,13 @@ def Uplaod(filename, extension):
     sftpclient.put(file, username + "/" + file) # Writes file to the user's folder.
     sftpclient.close() # Closes the sftp connection.
 
+
+
 ###
 ### Cleanup before exit.
 ###
 
-namegen = NameGen("footage", "mkv")
-print(namegen)
+Record()
 
 sshclient.close() # Close the ssh client.
 sys.exit(0) # Exit without error.
