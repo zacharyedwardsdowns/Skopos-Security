a6bc299bf11086565aa004e39cfee965f4f07ddc
ToddRob0 <45016883+ToddRob0@users.noreply.github.com>
2019-04-20 22:39:37 -0500

    Style Changes

--- Website/User/settings.php
+++ Website/User/settings.php
@@ -16,6 +16,11 @@
 	// Set their username.
 	$username = $_SESSION["username"];
 
+	// Store data into variables
+	$email =  $_POST['em'];
+	$password = $_POST['pass'];
+	$phonenumber = $_POST['phnnum'];
+	
 	// Check them against the database for a match.
     $sql = "SELECT sessionID FROM sessions WHERE username='$username';";
 	$result = mysqli_query($link, $sql);
@@ -27,73 +32,77 @@
     {
 	?>
 
-        <html>
-		<head>
-			<title>SKOPOS - <?php echo ucfirst($username); ?>'s Settings</title>
-			<link rel="stylesheet" type="text/css" href="register.css"/>
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-		</head>
-		<body style="background-color:rgba(12, 170, 149);">
+<html>
+    <head>
+        <title>SKOPOS - <?php echo ucfirst($username); ?>'s Settings</title>
+        <link rel="stylesheet" type="text/css" href="settings.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    </head>
+    <body style="background-color:rgba(12, 170, 149);">
 
-        <!--THIS BUTTON EXECUTES LOG OUT-->
-			<div class="col-sm-3">
-				<form action="logout.php">
-					<input type="submit" value="Logout" id="logout"/>
-				</form>
-			</div>
+    <ul>
+              <li><a href="https://skopossecurity.com/user/home">Home</a></li>
+              <li><a href="https://skopossecurity.com/user/schedule">Schedule</a></li>
+              <li><a class="active" href="https://skopossecurity.com/user/settings">Settings</a></li>
+            <li><form action="logout.php"><input type="submit" value="Logout" id="logout"/></form></li>
+        </ul>
 
-			<div class="container text-center" id="logo">
-				<a href="https://skopossecurity.com">
-					<img src="../Pictures/Logo.png" height="200" width="200"/>
-				</a>
-			</div>
+        			<!--THIS BUTTON EXECUTES LOG OUT-->
+			<form action="logout.php">
+                <input type="submit" value="Logout" id="logout"/>
+            </form>
 
-			<div class="container">
-				<div class="col-sm-6 text-center col-sm-offset-3" id="regbox">
-					<form method="post" action="https://www.skopossecurity.com/regaction">
-						<h2>Setting</h2>
-						<form action="/setting.php">
-  							<fieldset>
-    							<legend>Account information:</legend>
-    							Username:<br>
-   								<input type="text" id="username" placeholder="Username*" size="35" name="uname" />
-								<br><br>
-    							Password:<br>
-    							<input type="password" id="password" placeholder="Password*"  size="35" pattern="([a-zA-z0-9] |!|@|#|&|$|%|^|*)" maxlength="64" minlength="8" name="pass"/>
-   								<br><br>
-    							Confirm Password:<br>
-    							<input type="password" id="passwordcheck" placeholder="Confirm Password*" size="35" name="passcheck"/>
-    							<br><br>
-    							Phone Number:<br>
-    							<input type="text" id="phonenumber" placeholder="Phn: 1112223333" pattern="[0-9]{10}" size="35" maxlength="10" name="phnnum"/>
-								<br><br>
-    							Email:<br>
-    							<input type="email" id="email" placeholder="example@gmail.com*" size="35" name="em" />
-    							<br><br>
+ 
 
-    							<font color="brown"><input type="submit" value="Submit">
-    							<font color="blue"><button type="reset"  value="Reset">Reset</button>
+        <div class="container text-center" id="logo">
+            <a href="https://skopossecurity.com">
+                <img src="../Pictures/Logo.png" height="200" width="200"/>
+            </a>
+        </div>
 
-								<script>
-									function ConfirmDelete()
-									{
-  										confirm ("Are you sure you want to delete account?");
-									}
-								</script>
-									
-								<form name='del_update' action='settings.php?dealer_id=10&usnamer=1' method='post'/>
-									<font color="red"> <input type="submit" value="Delete Account" Onclick="ConfirmDelete()" />
-									<input type="hidden" name="id" value="41" />
-									<input type="hidden" name="url" value="settings.php?dealer_id=10&username=1" />
-								</form>
-  							</fieldset>
-						</form>
-					</form>
-				</div>
-			</div>
+        <div class="container">
+            <div class="col-sm-6 text-center col-sm-offset-3" id="regbox">
+                <form method="post" action="https://www.skopossecurity.com/regaction">
+                    <h2>Account Settings</h2>
+                    <form action="/setting.php">
+                          <fieldset>
+                            <legend>Account information:</legend>
+                            Update Password:<br>
+                            <input type="password" id="password" placeholder="Password*" size="35" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number,one uppercase and lowercase letter, and at least 8 or more characters" name="pass" required/>
+                               <br><br>
+                            Confirm New Password:<br>
+                            <input type="password" id="passwordcheck" placeholder="Confirm Password*" size="35" name="passcheck"/>
+                            <br><br>
+                            Update Phone Number:<br>
+                            <input type="text" id="phonenumber" placeholder="Phn: 1112223333" pattern="[0-9]{10}" size="35" maxlength="10" name="phnnum"/>
+                            <br><br>
+                            Update Email:<br>
+                            <input type="email" id="email" placeholder="example@gmail.com*" size="35" name="em" />
+                            <br><br>
 
-		</body>
-		</html>
+                            <input type="submit" value="Update" id="update">
+                            <button type="reset"  value="Reset" id="reset">Reset</button>
+
+                            <script>
+                                function ConfirmDelete()
+                                {
+                                      confirm ("Are you sure you want to delete account?");
+                                }
+                            </script>
+                                
+                            <form name='del_update' action='settings.php?dealer_id=10&usnamer=1' method='post'/>
+                                <input type="submit" value="Delete Account" id="delete" Onclick="ConfirmDelete()" />
+                                <input type="hidden" name="id" value="41" />
+                                <input type="hidden" name="url" value="settings.php?dealer_id=10&username=1" />
+                            </form>
+                          </fieldset>
+                    </form>
+                </form>
+            </div>
+        </div>
+
+    </body>
+    </html>
 
 	<?php
 	}
